bdaf1939c4279054cc196a7dc304f40678097580
raphael <lefevrerapha@gmail.com>
2024-08-23 19:21:49 +0200

    adapt

--- js/scripts.js
+++ js/scripts.js
@@ -184,7 +184,7 @@ $(document).ready(function () {
         },
         data: {
             // Event title
-            title: "Ram and Antara's Wedding",
+            title: "Charlène et Raphaël Marriage",
 
             // Event start date
             start: new Date('Nov 27, 2017 10:00'),
@@ -218,7 +218,7 @@ $(document).ready(function () {
             && MD5($('#invite_code').val()) !== '2ac7f43695eb0479d5846bb38eec59cc') {
             $('#alert-wrapper').html(alert_markup('danger', '<strong>Sorry!</strong> Your invite code is incorrect.'));
         } else {
-            $.post('https://script.google.com/macros/s/AKfycbyo0rEknln8LedEP3bkONsfOh776IR5lFidLhJFQ6jdvRiH4dKvHZmtoIybvnxpxYr2cA/exec', data)
+            $.post('https://script.google.com/macros/s/AKfycbyRwAAD_g0B_8SKFWmc-Z-rhIb5hG52L0q71A3pbFhTs16608KpzUWdGPWY3mQS2Ho/exec', data)
                 .done(function (data) {
                     console.log(data);
                     if (data.result === "error") {
